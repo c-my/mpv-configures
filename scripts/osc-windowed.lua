42c9067ffa17a368f07fba3e6ee83ae220d87d05
c-my <cmy1113@outlook.com>
2020-01-06 12:41:33 +0800

    initial commit

---
+++ scripts/osc-windowed.lua
@@ -0,0 +1,8 @@
+
+-- mp.observe_property("fullscreen", "bool", function(name, value)
+--     if value then
+--         mp.commandv("script-message", "osc-visibility", "auto", "no-osd")
+--     else
+--         mp.commandv("script-message", "osc-visibility", "always", "no-osd")
+--     end
+-- end)
